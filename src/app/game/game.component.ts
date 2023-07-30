45b42094bd21bb009290c0d62b2cf3fbc5dce5aa
Princeton_Evans <princetonevans@yahoo.com>
2023-07-30 15:43:24 -0500

    Added method for if strike

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
+  rolls: number[] = [];
 
+  roll(pins: number) {
+    this.rolls.push(pins);
+    console.log(pins);
+  }
+
+  getScore(): number {
+    let score = 0;
+    let rollIndex = 0;
+
+    for (let frame = 0; frame < 10; frame++) {
+      if (this.isStrike(rollIndex)) {
+        score += 10 + this.strikeBonus(rollIndex);
+        rollIndex += 1;
+      }
+    }
+
+    return score;
+  }
+
+  private isStrike(rollIndex: number): boolean {
+    return this.rolls[rollIndex] === 10;
+  }
+
+  private strikeBonus(rollIndex: number): number {
+    return this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  }
 }
