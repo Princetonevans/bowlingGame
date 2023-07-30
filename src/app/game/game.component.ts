08c1cb8acdadf75f99118af8064b55097603c087
Princeton_Evans <princetonevans@yahoo.com>
2023-07-30 15:45:24 -0500

    Added method for spare

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -21,6 +21,9 @@ export class GameComponent {
       if (this.isStrike(rollIndex)) {
         score += 10 + this.strikeBonus(rollIndex);
         rollIndex += 1;
+      } else if (this.isSpare(rollIndex)) {
+        score += 10 + this.spareBonus(rollIndex);
+        rollIndex += 2;
       }
     }
 
@@ -33,5 +36,13 @@ export class GameComponent {
 
   private strikeBonus(rollIndex: number): number {
     return this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  }
+
+  private isSpare(rollIndex: number): boolean {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1] === 10;
+  }
+
+  private spareBonus(rollIndex: number): number {
+    return this.rolls[rollIndex + 2];
   }
 }
